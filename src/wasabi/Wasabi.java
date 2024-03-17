b0534b5f36f6c338090a5569ea48b79361c3454a
hmmmquan <95281540+hmmmquan@users.noreply.github.com>
2024-03-16 19:11:19 -0700

    Initial commit

---
+++ src/wasabi/Wasabi.java
@@ -0,0 +1,61 @@
+package wasabi;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.input.Keyboard;
+
+public class Wasabi {
+
+    public static void main(String[] args) {
+        Wasabi basic = new Wasabi();
+        basic.start();
+    }
+    
+    public void start() {
+        try {
+            createWindow();
+            initGL();
+            render();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void createWindow() throws Exception {
+        Display.setFullscreen(false);
+        Display.setDisplayMode(new DisplayMode(640, 480));
+        Display.setTitle("Cult of Wasabi");
+        Display.create();
+    }
+    
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-320, 320, -240, 240, 1, -1); 
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+    
+    private void render() {
+        while (!Display.isCloseRequested()) {
+            try {
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                glLoadIdentity();
+
+                Display.update();
+                Display.sync(60);
+
+                while (Keyboard.next()) {
+                    if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE && Keyboard.getEventKeyState()) {
+                        Display.destroy();
+                        System.exit(0);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Display.destroy();
+    }
+}
