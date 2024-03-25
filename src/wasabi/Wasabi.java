b3b7cb68d91ede4082d609df4e1fc777bbf831d2
hmmmquan <95281540+hmmmquan@users.noreply.github.com>
2024-03-25 02:16:51 -0700

    checkpoint1

--- src/wasabi/Wasabi.java
+++ src/wasabi/Wasabi.java
@@ -1,10 +1,238 @@
 package wasabi;
+
+/***************************************************************
+* file: Wasabi.java
+* authors: Indigo Alvarado, Marcus Barga, Quan Nguyen
+* class: CS 4450 – Computer Graphics
+*
+* assignment: Semester Project
+* date last modified: 3/25/204
+*
+* purpose: to be written
+*
+****************************************************************/
+
+import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.util.glu.GLU;
 import static org.lwjgl.opengl.GL11.*;
-import org.lwjgl.input.Keyboard;
+import org.lwjgl.Sys;
+
+
+class Vector3Float {
+    public float x, y, z;
+
+    public Vector3Float(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class FPCameraController {
+    //3d vector to store the camera's position in
+    private Vector3f position = null;
+    private Vector3f lPosition = null;
+    //the rotation around the Y axis of the camera
+    private float yaw = 0.0f;
+    //the rotation around the X axis of the camera
+    private float pitch = 0.0f;       
+    private Vector3Float me;
+    
+    public FPCameraController(float x, float y, float z) {
+        //instantiate position Vector3f to the x y z params.
+        position = new Vector3f(x, y, z);
+        lPosition = new Vector3f(x,y,z);
+        lPosition.x = 0f;
+        lPosition.y = 15f;
+        lPosition.z = 0f;
+    }
+    
+    //increment the camera's current yaw rotation
+    public void yaw(float amount) {
+        //increment the yaw by the amount param
+        yaw += amount;
+    }
+    
+    //increment the camera's current yaw rotation
+    public void pitch(float amount) {
+        //increment the pitch by the amount param
+        pitch -= amount;
+    }
+    
+    //moves the camera forward relative to its current rotation (yaw)
+    public void walkForward(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw));
+        position.x-= xOffset;
+        position.z += zOffset;
+    }
+    
+    public void walkBackwards(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw));
+        position.x += xOffset;
+        position.z-= zOffset;
+    }
+    
+    public void strafeLeft(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw-90));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw-90));
+        position.x-= xOffset;
+        position.z += zOffset;
+    }
+
+    //strafes the camera right relative to its current rotation (yaw)
+    public void strafeRight(float distance) {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw+90));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw+90));
+        position.x-= xOffset;
+        position.z += zOffset;
+    }
+    
+     //moves the camera up relative to its current rotation (yaw)
+    public void moveUp(float distance) {
+        position.y-= distance; 
+    }
+    
+    //moves the camera down
+    public void moveDown(float distance) {
+        position.y += distance;
+    }
+
+    //translates and rotate the matrix so that it looks through the camera
+    //this does basically what gluLookAt() does
+    public void lookThrough() {
+        //roatate the pitch around the X axis
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        //roatate the yaw around the Y axis
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        //translate to the position vector's location
+        glTranslatef(position.x, position.y, position.z);
+    }
+    
+    public void gameLoop() {
+        FPCameraController camera = new FPCameraController(0, 0, 0);
+        float dx = 0.0f;
+        float dy = 0.0f;
+        float dt = 0.0f; //length of frame
+        float lastTime = 0.0f; // when the last frame was
+        long time = 0;
+        float mouseSensitivity = 0.09f;
+        float movementSpeed= .35f;
+        //hide the mouse
+        //Mouse.setGrabbed(true);
+        
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
+            time = Sys.getTime();
+            lastTime = time;
+            
+            //distance in mouse movement 
+            //from the last getDX() call.
+            dx = Mouse.getDX();
+            
+            //distance in mouse movement 
+            //from the last getDY() call.
+            dy = Mouse.getDY();
+            
+            //when passing in the distance to move
+            //we times the movementSpeedwith dt this is a time scale
+            //so if its a slow frame u move more then a fast frame
+            //so on a slow computer you move just as fast as on a fast computer
+            if (Keyboard.isKeyDown(Keyboard.KEY_W)) { //move forward
+                camera.walkForward(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_S)) { //move backwards
+                camera.walkBackwards(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_A)) { //strafe left        
+                camera.strafeLeft(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_D)) { //strafe right        
+                camera.strafeRight(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) { //move up        
+                camera.moveUp(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
+                camera.moveDown(movementSpeed);
+            }
+            
+            //set the modelview matrix back to the identity
+            glLoadIdentity();
+            //look through the camera before you draw anything
+            camera.lookThrough();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            //you would draw your scene here.
+            render();
+            //draw the buffer to the screen 
+            Display.update();
+            Display.sync(60);
+        }
+        Display.destroy();
+    }
+    
+    private void render() {
+        try {
+            glBegin(GL_QUADS);
+
+            //Top face (red)
+            glColor3f(1.0f, 0.0f, 0.0f); //Red
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+
+            // Bottom
+            glColor3f(0.0f, 1.0f, 0.0f); //Green
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+
+            // Front
+            glColor3f(0.0f, 0.0f, 1.0f); //Blue
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+
+            // Back
+            glColor3f(1.0f, 1.0f, 0.0f); //Yellow
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+
+            // Left
+            glColor3f(1.0f, 0.0f, 1.0f); //Magenta
+            glVertex3f(-1.0f, 1.0f, 1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, 1.0f);
+
+            //Right
+            glColor3f(0.0f, 1.0f, 1.0f); //Cyan
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+
+            glEnd();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
 
 public class Wasabi {
+    
+    private FPCameraController fp = new FPCameraController(0f,0f,0f);
+    private DisplayMode displayMode;
 
     public static void main(String[] args) {
         Wasabi basic = new Wasabi();
@@ -15,7 +243,7 @@ public class Wasabi {
         try {
             createWindow();
             initGL();
-            render();
+            fp.gameLoop();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -23,39 +251,25 @@ public class Wasabi {
     
     private void createWindow() throws Exception {
         Display.setFullscreen(false);
-        Display.setDisplayMode(new DisplayMode(640, 480));
+        DisplayMode[] d = Display.getAvailableDisplayModes();
+        for(int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640 && d[i].getHeight() == 480 && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
         Display.setTitle("Cult of Wasabi");
         Display.create();
     }
-    
+
     private void initGL() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(-320, 320, -240, 240, 1, -1); //center 
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth()/(float)displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
+        glEnable(GL_DEPTH_TEST);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-    }
-    
-    private void render() {
-        while (!Display.isCloseRequested()) {
-            try {
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                glLoadIdentity();
-
-                Display.update();
-                Display.sync(60);
-
-                while (Keyboard.next()) {
-                    if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE && Keyboard.getEventKeyState()) {
-                        Display.destroy();
-                        System.exit(0);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        Display.destroy();
     }
 }
